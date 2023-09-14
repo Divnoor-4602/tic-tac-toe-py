0f1efd5568e0c894b93914134fe516c624baff28
Divnoor <divnoorsingh4602@gmail.com>
2023-09-14 16:04:02 +0530

    Win check function in progress

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 
 inital_game_positions = np.array([[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]])
 symbols = {"cross": "X", "circle": "O"}
+winning_combinations = []
+
 
 space_line_count = 0
 vertical_bar_count = 0
@@ -26,20 +28,30 @@ def game_board_setup(game_array, initial_space_count, vertical_count):
             spaces_per_game()
 
 
-def place_character(user_position_choice, player_num):
-    position_chosen = user_position_choice.split(" ")
+def get_board_positions(user_choice_positions):
+    position_chosen = user_choice_positions.split(" ")
     row_chosen = int(position_chosen[0]) - 1
     column_chosen = int(position_chosen[1]) - 1
+    return row_chosen, column_chosen
+
+
+def place_character(current_user_choice, player_num):
+    row_current_user, column_current_user = get_board_positions(current_user_choice)
     if player_num == 1:
-        inital_game_positions[row_chosen][column_chosen] = "X"
+        inital_game_positions[row_current_user][column_current_user] = "X"
     elif player_num == 2:
-        inital_game_positions[row_chosen][column_chosen] = "O"
+        inital_game_positions[row_current_user][column_current_user] = "O"
     else:
         print("invalid player number")
 
 
-def win_check():
+def win_check(current_user_choice, current_positions):
     """Algorithm to check whose the winner"""
+    row_current_user, col_current_user = get_board_positions(current_user_choice)
+    # check if all elements in that row are the same
+    if len(set(current_positions[row_current_user])) == 1:
+        print("Winner")
+    # extracting elements from a 2d column matrix and saving them in a list
 
 
 game_board_setup(inital_game_positions, space_line_count, vertical_bar_count)
@@ -56,6 +68,7 @@ while start:
         )
         place_character(position_place, 1)
         game_board_setup(inital_game_positions, space_line_count, vertical_bar_count)
+        win_check(position_place, inital_game_positions)
         user = False
     elif user == False:
         print("Player 2\n")
